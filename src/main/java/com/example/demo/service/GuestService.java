7d4bfcd74436789927939781ecdd43234bd2f797
bala-amypo <bala@amypo.in>
2025-12-23 04:35:05 +0000

    User commit

--- src/main/java/com/example/demo/service/GuestService.java
+++ src/main/java/com/example/demo/service/GuestService.java
@@ -1,9 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Guest;
+
 import java.util.List;
 
 public interface GuestService {
-    Guest save(Guest guest);
-    List<Guest> getAll();
+
+    Guest createGuest(Guest guest);
+
+    List<Guest> getAllGuests();
+
+    Guest getGuestById(Long id);
+
+    Guest updateGuest(Long id, Guest guest);
+
+    void deleteGuest(Long id);
 }
--- src/main/java/com/example/demo/service/impl/GuestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/GuestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Guest;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.GuestRepository;
 import com.example.demo.service.GuestService;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,37 @@ public class GuestServiceImpl implements GuestService {
     }
 
     @Override
-    public Guest save(Guest guest) {
+    public Guest createGuest(Guest guest) {
         return guestRepository.save(guest);
     }
 
     @Override
-    public List<Guest> getAll() {
+    public List<Guest> getAllGuests() {
         return guestRepository.findAll();
+    }
+
+    @Override
+    public Guest getGuestById(Long id) {
+        return guestRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Guest not found with id " + id));
+    }
+
+    @Override
+    public Guest updateGuest(Long id, Guest guest) {
+        Guest existingGuest = getGuestById(id);
+
+        existingGuest.setFullName(guest.getFullName());
+        existingGuest.setEmail(guest.getEmail());
+        existingGuest.setPhoneNumber(guest.getPhoneNumber());
+        existingGuest.setActive(guest.isActive());
+
+        return guestRepository.save(existingGuest);
+    }
+
+    @Override
+    public void deleteGuest(Long id) {
+        Guest guest = getGuestById(id);
+        guestRepository.delete(guest);
     }
 }

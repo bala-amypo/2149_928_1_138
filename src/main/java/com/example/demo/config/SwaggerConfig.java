6c929772dbd865f375b1a9b640f704464d61a1aa
bala-amypo <bala@amypo.in>
2025-12-23 04:41:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Hotel Digital Key share")
+                        .title("Hotel Room Key Digital share")
                         .description("Swagger CRUD APIs")
                         .version("1.0"))
                 

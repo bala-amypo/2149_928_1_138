5b42ad390f5cee91b29bdc10e7c2730824b697f2
bala-amypo <bala@amypo.in>
2025-12-23 05:39:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/RoomBooking.java
+++ src/main/java/com/example/demo/entity/RoomBooking.java
@@ -1,6 +1,11 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Future;
+
 import java.time.LocalDate;
 
 @Entity
@@ -11,16 +16,25 @@ public class RoomBooking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String roomNumber;
+
+    @NotNull
+    @FutureOrPresent
     private LocalDate checkInDate;
+
+    @NotNull
+    @Future
     private LocalDate checkOutDate;
+
+    @NotNull
     private boolean active;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @NotNull
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "guest_id", nullable = false)
     private Guest guest;
 
-    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 

e5faa6dcf6cac08ddfa29e27b0a8ff4eeb2d28ed
bala-amypo <bala@amypo.in>
2025-12-26 12:55:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/AccessLogRepository.java
+++ src/main/java/com/example/demo/repository/AccessLogRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.*;
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
-    List<AccessLog> findByGuestId(Long guestId);
     List<AccessLog> findByDigitalKeyId(Long keyId);
+    List<AccessLog> findByGuestId(Long guestId);
 }
--- src/main/java/com/example/demo/repository/KeyShareRequestRepository.java
+++ src/main/java/com/example/demo/repository/KeyShareRequestRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.*;
 public interface KeyShareRequestRepository extends JpaRepository<KeyShareRequest, Long> {
-    List<KeyShareRequest> findBySharedWithId(Long guestId);
     List<KeyShareRequest> findBySharedById(Long guestId);
+    List<KeyShareRequest> findBySharedWithId(Long guestId);
 }
